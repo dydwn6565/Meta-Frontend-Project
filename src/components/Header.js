410edf7833b0987f7e7dc9c5a9f62cb1d1dbf6c0
YongjuLee <ivan.yongju.lee@gmail.com>
2023-11-15 11:18:36 -0800

    finished Nav

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='header'>
+        <section>
+            <div>
+                <h2>Little Lemon</h2>
+                <h3>Chicago</h3>
+                <p>We are a family woned Mediterraneran restaurant, focused on traditional recipes served with a modern twist.</p>
+            </div>
+        </section>
+    </header>
+  )
+}
+
+export default Header
