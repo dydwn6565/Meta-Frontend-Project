9b1d7e7ae8b18b15467eb970dae9443edb1ba714
YongjuLee <ivan.yongju.lee@gmail.com>
2023-11-15 11:21:38 -0800

    finished header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
+import bannerImg from "../images/restauranfood.jpg"
 function Header() {
   return (
     <header className='header'>
@@ -8,6 +9,12 @@ function Header() {
                 <h2>Little Lemon</h2>
                 <h3>Chicago</h3>
                 <p>We are a family woned Mediterraneran restaurant, focused on traditional recipes served with a modern twist.</p>
+                <Link to="">
+                    <button aria-label='On Click'>Reserve Table</button>
+                </Link>
+            </div>
+            <div className='banner-img'>
+                <img src={bannerImg} alt=""/>
             </div>
         </section>
     </header>
