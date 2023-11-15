0b3c4ec8bdd17929bc0fed437b91218d5dd78ad3
YongjuLee <ivan.yongju.lee@gmail.com>
2023-11-15 13:57:51 -0800

    completed footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Nav from './components/Nav';
 // import BookingForm from './components/BookingForm';
 import Main from './components/Main';
 import Menu from './components/Menu';
+import Footer from './components/Footer';
 
 function App() {
   return (
